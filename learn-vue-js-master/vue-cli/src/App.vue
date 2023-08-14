ccb22b9a3a0336c7a8a0ce5a3f8bea40a4aaab9f
boeunLee <bounjaeun@gmail.com>
2023-08-14 20:25:08 +0900

    섹션11: 컴포넌트 등록 및 props 전달

--- learn-vue-js-master/vue-cli/src/App.vue
+++ learn-vue-js-master/vue-cli/src/App.vue
@@ -1,8 +1,15 @@
 <template>
-  <div>{{ str }}</div>
+  <div>
+    {{ str }}
+    <!-- 프롭스 전달 -->
+    <!-- <app-header v-bind:하위 컴포넌트에서 정의한 프롭스 속성 이름="상위 컴포넌트의 데이터 이름"></app-header> -->
+    <app-header v-bind:propsdata="str"></app-header>
+  </div>
 </template>
 
 <script>
+import AppHeader from "./components/AppHeader.vue";
+
 export default {
   // 기존에 하던 방식과는 다르게 진행되어야함
   // 여러개의 컴포넌트를 만들었을 때 같은 이름을 공유하면 안되기 때문이다.
@@ -11,6 +18,9 @@ export default {
     return {
       str: "hi",
     };
+  },
+  components: {
+    "app-header": AppHeader,
   },
 };
 </script>
---
+++ learn-vue-js-master/vue-cli/src/components/AppHeader.vue
@@ -0,0 +1,15 @@
+<template>
+  <header>
+    <h1>{{ propsdata }}</h1>
+  </header>
+</template>
+
+<script>
+export default {
+  // instance 옵션
+  // 상위 컴포넌트에서 여기의 propsdata로 데이터를 내려줄 수 있다.
+  props: ["propsdata"],
+};
+</script>
+
+<style></style>
